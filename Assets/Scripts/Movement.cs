247cb9951d73c4640ff026d5da3eaa579ac4b2e0
Rishav Banerjee <thatamuzak@gmail.com>
2022-04-17 16:36:09 +0530

    reworked movement controls with forces

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -5,28 +5,51 @@ public class Movement : MonoBehaviour
 {
     [SerializeField] private float moveSpeed;
     [SerializeField] private float jumpForce;
+    [SerializeField] private float maxSpeed;
+
+    private Vector3 moveForce=Vector3.zero;
 
     private Rigidbody rb;
-    private Vector2 movementInput;
+    private Vector3 movementInput;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
         MovePlayer();
+        CounterMovement();
     }
 
     private void MovePlayer()
     {
-        rb.velocity = moveSpeed * (new Vector3(movementInput.x, rb.velocity.y, movementInput.y));
+        moveForce = movementInput*moveSpeed;
+        rb.AddForce(moveForce);
+        rb.velocity = Vector3.ClampMagnitude(rb.velocity, maxSpeed); 
+    }
+
+    private void CounterMovement()
+    {
+        bool noInput = Mathf.Approximately(Vector3.SqrMagnitude(movementInput), 0);
+        bool oppositeInput = Vector3.Dot(rb.velocity, movementInput) <= 0;
+        if (noInput)
+        {
+            Vector3 counterForce = rb.velocity * (-1f * 0.99f);
+            rb.AddForce(counterForce);
+        }
+        else if (oppositeInput)
+        {
+            rb.AddForce(moveForce*3f);
+        }
     }
     
     public void OnMovementInput(InputAction.CallbackContext ctx)
     {
-        movementInput = ctx.ReadValue<Vector2>();
+        Vector2 movementInput2d = ctx.ReadValue<Vector2>();
+        movementInput2d=movementInput2d.normalized;
+        movementInput = new Vector3(movementInput2d.x, 0, movementInput2d.y);
         Debug.Log(movementInput);
     }
 
