6a2160e3320f012c1ead56e0cc72139083fdf225
Rishav Banerjee <thatamuzak@gmail.com>
2022-04-17 21:54:19 +0530

    Basic Data Logger Created

--- Assets/Scripts/DataLogger.cs
+++ Assets/Scripts/DataLogger.cs
@@ -1,9 +1,40 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class DataLogger : MonoBehaviour
 {
     [SerializeField] private GameObject player;
+
+    private TextWriter tw;
+    private string fileName;
+    private Vector3 playerLoc;
+    private Vector3 playerRot;
+
+    private void Start()
+    {
+        playerLoc = player.transform.position;
+        playerRot = player.transform.eulerAngles;
+        fileName = Application.dataPath + "/test.csv";
+        tw = new StreamWriter(fileName, false);
+        tw.WriteLine("Time, Pos.x, Pos.y, Pos.z, Rot.x, Rot.y, Rot.z");
+    }
     
+    private void Update()
+    {
+        WriteCsv();
+    }
+
+    public void KillLogging(InputAction.CallbackContext ctx)
+    {
+        tw.Close();
+    }
+    
+    private void WriteCsv()
+    {
+        tw.WriteLine(Time.timeSinceLevelLoad+","+playerLoc.x+","+playerLoc.y+","+playerLoc.z+","+playerRot.x+","+playerRot.y+","+playerRot.z+",");
+    }
 }
