96a5b2e493c599bea477e1495429f4f11cc6786e
Rishav Banerjee <thatamuzak@gmail.com>
2022-04-19 03:32:26 +0530

    Fixed Player pose data update logic

--- Assets/Scripts/DataLogger.cs
+++ Assets/Scripts/DataLogger.cs
@@ -8,11 +8,12 @@ using UnityEngine.InputSystem;
 
 public class DataLogger : MonoBehaviour
 {
-    [SerializeField] private GameObject player;
+    [SerializeField] private Transform playerTransform;
     [Range(1,100)]
     [Description("per second")]
     [SerializeField] private int resolution;
 
+    private Coroutine routine;
     private float waitTime;
     private TextWriter tw;
     private string fileName;
@@ -23,37 +24,42 @@ public class DataLogger : MonoBehaviour
 
     private void Start()
     {
-        waitTime = 1 / resolution;
+        waitTime = 1.0f / resolution;
         data = new List<Array>();
         singleFrame = new float[7];
-        playerLoc = player.transform.position;
-        playerRot = player.transform.eulerAngles;
         fileName = Application.dataPath + "/test.csv";
         tw = new StreamWriter(fileName, false);
         tw.WriteLine("Time, Pos.x, Pos.y, Pos.z, Rot.x, Rot.y, Rot.z");
         tw.Close();
 
-        StartCoroutine(CallLogger(waitTime));
+        routine=StartCoroutine(CallLogger(waitTime));
     }
 
-    private IEnumerator CallLogger(float waitTime)
+    private void Update()
+    {
+        playerLoc = playerTransform.position;
+        playerRot = playerTransform.eulerAngles;
+    }
+
+    private IEnumerator CallLogger(float timeToWait)
     {
         while (true)
         {
             LogData();
-            yield return new WaitForSeconds(waitTime);
+            yield return new WaitForSeconds(timeToWait);
         }
     }
 
     public void KillLogging(InputAction.CallbackContext ctx)
     {
+        StopCoroutine(routine);
         tw = new StreamWriter(fileName, true);
+        Debug.Log(data.Count);
         foreach (Array array in data)
         {
             float[] frame = (float[]) array;
-            // Debug.Log(frame[0]+", "+frame[1]);
+            tw.WriteLine(frame[0]+","+frame[1]+","+frame[2]+","+frame[3]+","+frame[4]+","+frame[5]+","+frame[6]);
         }
-        
         Debug.Log("ended logging session");
         tw.Close();
     }
@@ -64,7 +70,7 @@ public class DataLogger : MonoBehaviour
     private void LogData()
     {
         Array.Clear(singleFrame, 0, singleFrame.Length);
-        Debug.Log("Called" + Time.realtimeSinceStartup);
+        
         singleFrame[0] = Time.timeSinceLevelLoad;
         singleFrame[1] = playerLoc.x;
         singleFrame[2] = playerLoc.y;
@@ -72,7 +78,7 @@ public class DataLogger : MonoBehaviour
         singleFrame[4] = playerRot.x;
         singleFrame[5] = playerRot.y;
         singleFrame[6] = playerRot.z;
-        
+
         data.Add(singleFrame);
     }
 }
