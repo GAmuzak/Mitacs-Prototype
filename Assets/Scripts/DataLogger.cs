5936062594774538643aa78560234ab6cd8bdea2
Rishav Banerjee <thatamuzak@gmail.com>
2022-04-20 11:19:37 +0530

    Data Logging Complete

--- Assets/Scripts/DataLogger.cs
+++ Assets/Scripts/DataLogger.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.IO;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -9,24 +8,22 @@ using UnityEngine.InputSystem;
 public class DataLogger : MonoBehaviour
 {
     [SerializeField] private Transform playerTransform;
-    [Range(1,100)]
-    [Description("per second")]
-    [SerializeField] private int resolution;
+    [Range(0.1f,100)][Tooltip("Sampling rate per second")]
+    [SerializeField] private float resolution;
 
     private Coroutine routine;
+    private bool loggingKilled;
     private float waitTime;
     private TextWriter tw;
     private string fileName;
     private Vector3 playerLoc;
     private Vector3 playerRot;
     private List<Array> data;
-    private float[] singleFrame;
 
     private void Start()
     {
         waitTime = 1.0f / resolution;
         data = new List<Array>();
-        singleFrame = new float[7];
         fileName = Application.dataPath + "/test.csv";
         tw = new StreamWriter(fileName, false);
         tw.WriteLine("Time, Pos.x, Pos.y, Pos.z, Rot.x, Rot.y, Rot.z");
@@ -45,22 +42,24 @@ public class DataLogger : MonoBehaviour
     {
         while (true)
         {
-            LogData();
             yield return new WaitForSeconds(timeToWait);
+            LogData();
         }
     }
 
     public void KillLogging(InputAction.CallbackContext ctx)
     {
+        if (loggingKilled) return;
+        loggingKilled = true;
         StopCoroutine(routine);
         tw = new StreamWriter(fileName, true);
-        Debug.Log(data.Count);
         foreach (Array array in data)
         {
             float[] frame = (float[]) array;
             tw.WriteLine(frame[0]+","+frame[1]+","+frame[2]+","+frame[3]+","+frame[4]+","+frame[5]+","+frame[6]);
         }
-        Debug.Log("ended logging session");
+
+        Debug.Log("-------------ended logging session-------------\nWARNING: No further information will be recorded for this session!");
         tw.Close();
     }
     
@@ -69,8 +68,7 @@ public class DataLogger : MonoBehaviour
     /// </summary>
     private void LogData()
     {
-        Array.Clear(singleFrame, 0, singleFrame.Length);
-        
+        float[] singleFrame = new float[7];
         singleFrame[0] = Time.timeSinceLevelLoad;
         singleFrame[1] = playerLoc.x;
         singleFrame[2] = playerLoc.y;
@@ -78,7 +76,6 @@ public class DataLogger : MonoBehaviour
         singleFrame[4] = playerRot.x;
         singleFrame[5] = playerRot.y;
         singleFrame[6] = playerRot.z;
-
         data.Add(singleFrame);
     }
 }
